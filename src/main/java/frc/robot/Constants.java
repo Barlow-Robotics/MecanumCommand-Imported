fd27ee0234e4929f614670ee4a57ddadda04c59b
cmroby <60613912+cmroby@users.noreply.github.com>
2022-01-11 06:58:27 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -95,7 +95,8 @@ public final class Constants {
   }
 
   public static final class OIConstants {
-    public static final int kDriverControllerPort = 1;
+    public static final int kDriverControllerPort = 0;
+    public static final int halfSpeedButton = 2;
   }
 
   public static final class AutoConstants {
