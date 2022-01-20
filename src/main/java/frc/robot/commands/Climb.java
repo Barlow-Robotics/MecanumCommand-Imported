ed9daf203512973b1e9d6ee05261e6a541e8dc0e
DrowsyBuzzard17 <47873652+DrowsyBuzzard17@users.noreply.github.com>
2022-01-20 15:09:26 -0500

    Added template for climb.java

---
+++ src/main/java/frc/robot/commands/Climb.java
@@ -0,0 +1,24 @@
+//No copyright? Nothing was in here when I opened it
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ArmBar;
+
+public class Climb extends CommandBase {
+    public Climb() {}
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {}
+
+    @Override
+    public void end(boolean interrupted) {}
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+}
