9fdf81248c3924163716056164529adc3dad0c64
cmroby <60613912+cmroby@users.noreply.github.com>
2022-02-07 16:21:38 -0500

    Wheel then Belt in Index/Shooter

--- src/main/java/frc/robot/subsystems/ShooterIndex.java
+++ src/main/java/frc/robot/subsystems/ShooterIndex.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.ShooterConstants;
 
 public class ShooterIndex extends SubsystemBase {
   /** Creates a new Shooter. */
@@ -41,8 +42,12 @@ public class ShooterIndex extends SubsystemBase {
   }
 
   public void startShooting() {
-    beltMotor.set(TalonFXControlMode.Velocity, Constants.ShooterConstants.BeltMotorShootingVelocity);
-    flyWheelMotor.set(TalonFXControlMode.Velocity, Constants.ShooterConstants.FlyWheelMotorShootingVelocity);
+    if(Math.abs(flyWheelMotor.get() - ShooterConstants.FlyWheelMotorShootingVelocity) > ShooterConstants.FlyWheelShootingTolerance) {
+      beltMotor.set(TalonFXControlMode.Velocity, Constants.ShooterConstants.BeltMotorShootingVelocity);
+    } 
+    else {
+      flyWheelMotor.set(TalonFXControlMode.Velocity, Constants.ShooterConstants.FlyWheelMotorShootingVelocity);
+    }
   }
 
   public void stopShooting() {
